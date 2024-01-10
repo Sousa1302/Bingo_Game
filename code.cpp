5f2c00e42e36bdfd3a09800c4fad3733dde11e85
Sousa1302 <sousaraposo1302@gmail.com>
2024-01-10 17:26:34 -0100

    Last num generation

--- code.cpp
+++ code.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 
 int nums_card[5][5];
-int amount_nums, random_nums_card, type_generator, random_nums_generated;
-char keep_generating;
+int amount_nums, random_nums_card, type_generator, number_generated = 0, random_nums_generated, last_num_generated = 0;
+char keep_generating = 'n';
 
 
 void bubbleSort(int arr[], int n) {
@@ -41,7 +41,7 @@ int main(){
     cin >> amount_nums;
 
     cout << "Do you want an automatic or a manual generator?\n";
-    cout << "A. Automatic \nB. Manual\n";
+    cout << "1. Automatic \n2. Manual\n";
     cin >> type_generator;
 
     ofstream ficheiro("Card.txt");
@@ -124,9 +124,26 @@ int main(){
         cout << "You must type a number between 1-3!\n";
     }
     
-    // cout << "The last number: "  << ultimo numero << endl;
-    // cout << "The number generated: " << numero agr <<  endl;
-    
+    /*do {
+    Nums_Generator(amount_nums);
+
+    if (number_generated != 0) {
+        cout << "Last number: " << last_num_generated << endl; // Exibe o último número gerado após a primeira iteração
+    }
+
+    last_num_generated = number_generated; // Armazena o último número gerado
+    number_generated = random_nums_generated; // Gera o próximo número
+
+    cout << "Number generated: " << number_generated << endl;
+    cout << "Do you wish to generate another number ?\n";
+    cout << "y / n\n";
+    cin >> keep_generating;
+
+    } while (keep_generating == 'y' || keep_generating == 'Y');
+
+    cout << "Last number: " << last_num_generated << endl; // Exibe o último número gerado antes de sair do loop
+    */
+
 
     return 0;
 }
